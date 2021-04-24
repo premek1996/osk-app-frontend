9f9998025f71a5fec20ddba0dbd9ceb1eb28722c
Patryk Ostaszewski <patryk.ostaszewski.ext@msg.group>
2021-04-24 10:24:28 +0200

    main-nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,45 @@ import {HttpClientModule} from '@angular/common/http';
 import { AuthorizationComponent } from './authorization/authorization.component';
 import { GooglePayComponent } from './google-pay/google-pay.component';
 import { GooglePayButtonModule } from '@google-pay/button-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorizationComponent,
-    GooglePayComponent
+    GooglePayComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     SocialLoginModule,
     HttpClientModule,
     ReactiveFormsModule,
-    GooglePayButtonModule
+    GooglePayButtonModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [
     CustomerService,
